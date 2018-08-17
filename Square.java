0afda3cdb75c9e2177dff3fb328d08dddff1e829
Sripathi <manjulavani.sripathi@capgemini.com>
2018-08-17 09:09:32 -0400

    first commit

---
+++ Square.java
@@ -0,0 +1,15 @@
+package assignment1;
+
+import java.util.*;
+public class Square
+{
+	public static void main (String args[])
+	{
+		Scanner sc=new
+		Scanner(System.in);
+		int num;
+		System.out.println("Enter an integer number:");
+		num=sc.nextInt();
+		System.out.println("Square of "+ num + " is: "+Math.pow(num,2));
+	}
+}
